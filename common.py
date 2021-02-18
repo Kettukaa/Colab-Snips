67cfa6e4b6b1e15474720b694d0c29dc97b95094
Kettukaa <33789207+Kettukaa@users.noreply.github.com>
2021-02-17 19:52:53 -0500

    Updated order of functions in common.py

--- common.py
+++ common.py
@@ -2,6 +2,16 @@ import imageio
 import torch
 import cv2
 
+def torch2uint8(images, permute_order=[0,2,3,1]):
+    r"""Convert batch of torch samples into a NumPy image array.
+    Arguments:
+        images (tensor): raw image samples.
+        permute_order: adjusts the permute order of tensor.permute()
+    Returns:
+        numpy array of images. 
+    """
+    return (images.permute(*permute_order)*127.5 + 128).clamp(0,255).to(torch.uint8).cpu().numpy()
+
 def batched_synthesis_generator(W, G, batch_size):
     r"""Generator that yields batches of images from W vectors.
     Arguments:
@@ -14,16 +24,6 @@ def batched_synthesis_generator(W, G, batch_size):
         images = G.synthesis(batch, noise_mode='const', force_fp32=True)
         images = torch2uint8(images)
         yield images
-
-def torch2uint8(images, permute_order=[0,2,3,1]):
-    r"""Convert batch of torch samples into a NumPy image array.
-    Arguments:
-        images (tensor): raw image samples.
-        permute_order: adjusts the permute order of tensor.permute()
-    Returns:
-        numpy array of images. 
-    """
-    return (images.permute(*permute_order)*127.5 + 128).clamp(0,255).to(torch.uint8).cpu().numpy()
 
 def batched_generator(Z, G, batch_size, truncation_psi):
     r"""Generator that yields batches of images from Z vectors.
